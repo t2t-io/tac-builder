5d3826895bf201a76cc4dd092b1df20954ebfb38
arronf <arronf@mac.com>
2017-11-19 16:03:41 +0800

    fix pug issue and docker update script

--- dockerfile/update.sh
+++ dockerfile/update.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # this script updates tac-builder project inside the container and commit to teh image t2t/tac-builder
-docker run -it t2t/tac-builder npm uninstall -g tac-builder && npm i -g git+https://github.com/t2t-io/tac-builder.git && sleep 1
+# docker run -it t2t/tac-builder npm uninstall -g tac-builder && npm i -g git+https://github.com/t2t-io/tac-builder.git && sleep 1
+# due to a npm install bug in 5.x, we need to do git clone and npm i -g locally
+docker run -it t2t/tac-builder npm i -g git+https://github.com/t2t-io/tac-builder.git
 docker commit `docker ps -lq` t2t/tac-builder
 docker rm `docker ps -lq`
